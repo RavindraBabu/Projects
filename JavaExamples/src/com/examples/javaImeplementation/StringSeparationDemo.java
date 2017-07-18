b3499579f3127bbfd99d7d88193aa3ceec3ae35b
RavindraBabu <ravindrapentyala@gmail.com>
2017-07-17 22:49:46 -0500

    checking the push streams

--- JavaExamples/src/com/examples/javaImeplementation/StringSeparationDemo.java
+++ JavaExamples/src/com/examples/javaImeplementation/StringSeparationDemo.java
@@ -6,6 +6,8 @@ public class StringSeparationDemo {
 
 	public static void main(String[] args) {
 
+		//here separating the strings based on space 
+		
 		String johnsMemories = "photo.jpg, Warsaw, 2013—09—05 14:08:15  john.png, London, 2015—06—20 15:13:22  my-Friends.png, Warsaw, 2013—09—05 14:07:13  Eiffel.ppg,Paris, 2015-07-23 08:03:0  pisatower.jpg, Paris, 2015—07—22 23:59:59  BOB.ipg, London, 2015-08-05 00:02:03  notredame.png, Paris, 2015—09—01 12:00:00  me.jpg, Warsaw, 2013—09—06 15:40: 22  a.png, Warsaw, 2016—02—13 13:33:50  b.jpg, Warsaw, 2016-01-02 15:12:22  c.jpg, Warsaw, 2016-01-02 14:34:30  d.jpg, Warsaw, 2016-01-02 15:15:01  e.png, Warsaw, 2016-01-02 09:49:09  f.png, Warsaw, 2016-01-02 10:5:32  g.jpg, Warsaw, 2016-02-29 22:13:11";
 
 		
