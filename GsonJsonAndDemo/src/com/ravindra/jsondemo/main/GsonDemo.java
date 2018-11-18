690c68a0d2fadcaeb069f80585fa936a6195d939
RavindraBabu <ravindrapentyala@gmail.com>
2018-11-18 10:41:04 -0600

    commiting branch

--- GsonJsonAndDemo/src/com/ravindra/jsondemo/main/GsonDemo.java
+++ GsonJsonAndDemo/src/com/ravindra/jsondemo/main/GsonDemo.java
@@ -6,8 +6,6 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
-import javax.jws.soap.SOAPBinding.Use;
-
 import com.google.gson.Gson;
 import com.ravindra.jsondemo.users.GsonUserDemo;
 
